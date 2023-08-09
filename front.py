56c16eaa70cda576f1b4dc85830aadf30bfbb563
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2023-08-09 16:23:56 -0300

    Update front.py

--- front.py
+++ front.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
 
 st.title("Olá mundo")
-df = pd.DataFrame({'Nomes':['Gabriela', 'Julia', 'Bruno', 'Sara'],'Idades': [22,23,45,26]})
-st.write(df)
-st.table(df)
-df
-st.dataframe(df.style.highlight_max())
+#df = pd.DataFrame({'Nomes':['Gabriela', 'Julia', 'Bruno', 'Sara'],'Idades': [22,23,45,26]})
+#st.write(df)
+#st.table(df)
+#df
+#st.dataframe(df.style.highlight_max())
+
+map_data = pd.DataFrame(np.random.randn(1000,2) / [50,50] + [37.76, 0122.4],
+                        columns = [ 'lat', 'lon'] )
+
+st.map(map_data)
