c1e662e3717cfd83adb4a26c0b651e8f8304e573
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2023-08-09 16:14:37 -0300

    Update front.py

--- front.py
+++ front.py
@@ -1,2 +1,6 @@
-import streamlit
-streamlit.title("Olá mundo")
+import streamlit as st
+import pandas a pd
+
+st.title("Olá mundo")
+df = pd.DataFrame({'Nomes':['Gabriela', 'Julia', 'Bruno', 'Sara'],'Idades': [22,23,45,26]})
+st.write(df)
